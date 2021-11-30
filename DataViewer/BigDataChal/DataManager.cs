56ee2998832efc7f201c90a79a97c458c0433963
YongheeLee <yhhhlee@gmail.com>
2021-12-01 00:02:08 +0900

    add dataviewer parsed company infos

---
+++ DataViewer/BigDataChal/DataManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDataChal
+{
+    public class DataManager
+    {
+        private static DataManager mgr = null;
+        public static DataManager Instance
+        {
+            get
+            {
+                if (mgr == null)
+                    mgr = new DataManager();
+                return mgr;
+            }
+        }
+
+        private ObservableCollection<CompanyInfoT> comInfos = new ObservableCollection<CompanyInfoT>();
+        public ObservableCollection<CompanyInfoT> ComInfos { get => comInfos; }
+
+        public void LoadCSV(string companyFile, string serviceFile, string jobFile)
+        {
+            if (!string.IsNullOrEmpty(companyFile))
+            {
+                using (System.IO.StreamReader sr = System.IO.File.OpenText(companyFile))
+                {
+                    string line = sr.ReadLine();
+
+                    string fullLine = null;
+                    bool inContent = false;
+                    string sub = null;
+                    List<string> data = new List<string>();
+
+                    while (true)
+                    {
+                        line = sr.ReadLine();
+                        if (line == null)
+                            break;
+
+                        for (int i = 0; i < line.Length; ++i)
+                        {
+                            if (!inContent && line[i] == ',')
+                            {
+                                data.Add(sub);
+                                sub = null;
+                            }
+                            else if (line[i] == '\"')
+                            {
+                                inContent = !inContent;
+                                sub += line[i];
+                            }
+                            else
+                            {
+                                sub += line[i];
+                            }
+                        }
+
+                        if (data.Count == 18)
+                            data.Add(sub);
+
+                        if (data.Count == 19)
+                        {
+                            CompanyInfoT info = new CompanyInfoT
+                            {
+                                ID = int.Parse(data[0]),
+                                KorName = data[1],
+                                EngName = data[2],
+                                ShortIntro = data[3],
+                                FullIntro = data[4],
+                                FoundDate = data[5],
+                                EmployeeCnt = data[6],
+                                Invsm = data[7],
+                                Homepage = data[8],
+                                SNS = data[9],
+                                RcmmnCnt = data[10],
+                                RcmmnCont = data[11],
+                                BusinessCat = data[12],
+                                Address = data[13],
+                                Technology = data[14],
+                                NewsCompany = data[15],
+                                NewsDate = data[16],
+                                NewsTitle = data[17],
+                                NewsLink = data[18],
+                            };
+
+                            comInfos.Add(info);
+                            inContent = false;
+                            sub = null;
+                            data.Clear();
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DataViewer/BigDataChal/DataModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDataChal
+{
+    public class BaseT : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+
+    public class CompanyInfoT : BaseT
+    {
+        private int id = -1;
+        private string korName = null;
+        private string engName = null;
+        private string shortIntro = null;
+        private string fullIntro = null;
+        private string foundDate = null;
+        private string employeeCnt = null;
+        private string accml_invsm_attrt_amt = null;
+        private string homepage = null;
+        private string sns = null;
+        private string entrp_rcmmn_cnt = null;
+        private string entrp_rcmmn_cont = null;
+        private string businessCat = null;
+        private string address = null;
+        private string technology = null;
+        private string newsCompany = null;
+        private string newsDate = null;
+        private string newsTitle = null;
+        private string newsLink = null;
+
+        public int ID { get { return id; } set { id = value; OnChanged("ID"); } }
+        public string KorName { get { return korName; } set { korName = value; OnChanged("ID"); } }
+        public string EngName { get { return engName; } set { engName = value; OnChanged("ID"); } }
+        public string ShortIntro { get { return shortIntro; } set { shortIntro = value; OnChanged("ID"); } }
+        public string FullIntro { get { return fullIntro; } set { fullIntro = value; OnChanged("ID"); } }
+        public string FoundDate { get { return foundDate; } set { foundDate = value; OnChanged("ID"); } }
+        public string EmployeeCnt { get { return employeeCnt; } set { employeeCnt = value; OnChanged("ID"); } }
+        public string Invsm { get { return accml_invsm_attrt_amt; } set { accml_invsm_attrt_amt = value; OnChanged("ID"); } }
+        public string Homepage { get { return homepage; } set { homepage = value; OnChanged("ID"); } }
+        public string SNS { get { return sns; } set { sns = value; OnChanged("ID"); } }
+        public string RcmmnCnt { get { return entrp_rcmmn_cnt; } set { entrp_rcmmn_cnt = value; OnChanged("ID"); } }
+        public string RcmmnCont { get { return entrp_rcmmn_cont; } set { entrp_rcmmn_cont = value; OnChanged("ID"); } }
+        public string BusinessCat { get { return businessCat; } set { businessCat = value; OnChanged("ID"); } }
+        public string Address { get { return address; } set { address = value; OnChanged("ID"); } }
+        public string Technology { get { return technology; } set { technology = value; OnChanged("ID"); } }
+        public string NewsCompany { get { return newsCompany; } set { newsCompany = value; OnChanged("ID"); } }
+        public string NewsDate { get { return newsDate; } set { newsDate = value; OnChanged("ID"); } }
+        public string NewsTitle { get { return newsTitle; } set { newsTitle = value; OnChanged("ID"); } }
+        public string NewsLink { get { return newsLink; } set { newsLink = value; OnChanged("ID"); } }
+    }
+}
