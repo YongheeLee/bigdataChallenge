10fded7c78b77660819de5a9122fa8a9e91cb94b
YongheeLee <yhhhlee@gmail.com>
2021-12-08 00:39:33 +0900

    add py file for cluster just import library

---
+++ Clustering/cluster.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from kmodes.kmodes import KModes
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import SpectralClustering
+
+#import csv
+
+
+#extract index and data
+
+
+#fit 4 models
+
